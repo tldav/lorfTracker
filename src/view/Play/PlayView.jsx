1ecb565ec4bec099903a46874466a61107cfc6fa
tldav <tldavis09@yahoo.com>
2024-04-19 15:14:13 -0500

    Implement wake lock and menu btn for play view

--- src/view/Play/PlayView.jsx
+++ src/view/Play/PlayView.jsx
@@ -1,8 +1,25 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+import useWakeLock from "react-use-wake-lock";
 import PlayerZone from "./region/PlayerZone/PlayerZone";
+import MenuBtn from "./region/MenuBtn/MenuBtn";
 
 function PlayView() {
+	const { request, release } = useWakeLock();
+	const { pathname } = useLocation();
+
+	useEffect(() => {
+		request();
+	});
+
+	useEffect(() => {
+		return () => release();
+	}, [pathname]);
+
 	return (
 		<div id="play-view">
+			<MenuBtn />
 			<PlayerZone player="player-two" />
 			<PlayerZone player="" />
 		</div>
