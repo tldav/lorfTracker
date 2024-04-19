27340c093b15edf33e14e1a9739e672874223bca
tldav <tldavis09@yahoo.com>
2024-04-19 17:13:22 -0500

    add pwa manifest

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,22 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), VitePWA()],
+	plugins: [
+		react(),
+		VitePWA({
+			manifest: {
+				icons: [
+					{
+						src: "/android-chrome-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+				],
+				theme_color: "ffffff",
+			},
+		}),
+	],
 	server: {
 		port: 3000,
 	},
